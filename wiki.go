4a53265810aee7962acc3f926f6f2cb0b9af2b55
fredde <kreti@mailbox.org>
2019-12-08 19:43:53 +0100

    Initial commit

---
+++ wiki.go
@@ -0,0 +1,71 @@
+/*
+
+*/
+ */
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func (p *Page) defaults() {
+	p.Body = []byte("Default page")
+	p.Title = "Default title"
+}
+
+func renderTemplate(w http.ResponseWriter, useTemplate string, p *Page) {
+	t, _ := template.ParseFiles(useTemplate + ".html")
+	t.Execute(w,p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request)  {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w,"view",p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request)  {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{
+			Title: title,
+		}
+	}
+	renderTemplate(w,"edit",p)
+}
+
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil,err
+	}
+
+	return &Page{
+		Title: title,
+		Body:  body,
+	}, nil
+}
+
+
+func main() {
+	http.HandleFunc("/view/",viewHandler)
+	http.HandleFunc("/edit/",editHandler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+}
